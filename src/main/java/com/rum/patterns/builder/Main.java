4990129ad4a43c2b8f0a42aae66f7b1a2e29d70d
Ernest Rum <ernest.rum@gmail.com>
2017-11-04 19:16:58 +0300

    code refactor

--- src/main/java/com/rum/patterns/builder/Main.java
+++ src/main/java/com/rum/patterns/builder/Main.java
@@ -2,11 +2,12 @@ package com.rum.patterns.builder;
 
 public class Main {
 
-    private User user;
-
     private void runMain() {
-        user = new User.UserBuilder("firstName1", "lastName2").phone("12345").address("address1").build();
-        System.out.println(user);
+        User user1 = new User.UserBuilder("firstName1", "lastName1").age(50).phone("12345").address("address1").build();
+        System.out.println(user1);
+
+        User user2 = new User.UserBuilder("firstName2", "lastName2").address("address2").build();
+        System.out.println(user2);
     }
 
     public static void main(String[] args) {
